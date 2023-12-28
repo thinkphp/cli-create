c412d695bd160a2b5851fafbd0805014f8b613fd
Adrian Statescu <mergesortv@gmail.com>
2023-12-28 23:43:04 +0200

    Update cli-create-ruby.go

--- cli-create-ruby.go
+++ cli-create-ruby.go
@@ -11,24 +11,10 @@ import (
 
 //will add more stuff we needed
 var content = `
+def main 
+    print "Hello World!"
+end
 
-"""
-Module Docstring
-"""
-
-__author__ = "Your Name"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
-
-def main():
-    """ Main entry point of the app """
-    print("hello world")
-
-
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
 `
 
 func main() {
